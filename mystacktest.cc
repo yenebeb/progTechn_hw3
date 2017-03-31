e135e90213547bcc38e3dc649f4f6eaf93b0e3c9
Y.K.A.G. Schwarze <s1908928@leidenuniv.nl>
2017-03-31 15:40:47 +0200

    update mystack

--- mystacktest.cc
+++ mystacktest.cc
@@ -35,14 +35,91 @@ BOOST_AUTO_TEST_CASE(mystack_push)
 }
 
 /* TODO: more tests here ... */
-BOOST_AUTO_TEST_CASE(push_multiple)
+BOOST_AUTO_TEST_CASE(mystack_push_multiple)
 {
-  Mystack<int> stack;
-  vector<int> vector{10, 20, 30};
+  MyStack<int> stack;
+  std::vector<int> vector{10, 20, 30};
   stack.push_multiple(vector.begin(), vector.end());
   BOOST_CHECK_EQUAL(stack.peek(), 30);
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.size(), 20);
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.peek(), 10); 
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.is_empty(), true);
+}
+
+BOOST_AUTO_TEST_CASE(mystack_push_multiple2)
+{
+  MyStack<int> stack;
+  stack.push_multiple({10, 20, 30, 40});
+  BOOST_CHECK_EQUAL(stack.size(), 4UL);
+  BOOST_CHECK_EQUAL(stack.peek(), 40);
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.peek(), 30);
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.peek(), 20);
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.peek(), 10);
+}
+
+BOOST_AUTO_TEST_CASE(mystack_peek)
+{
+  MyStack<int> stack;
+  //BOOST_CHECK_THROW(stack.peek(), std::out_of_range_error);
+  stack.push(10);
+  BOOST_CHECK_EQUAL(stack.peek(), 10);
+  stack.push(20);
+  BOOST_CHECK_EQUAL(stack.peek(), 20);
+  stack.pop();
+ // stack.pop();
+ // BOOST_CHECK_THROW(stack.peek(),std::out_of_range_error);
+}
+
+BOOST_AUTO_TEST_CASE(mystack_pop)
+{
+  MyStack<int> stack;
+  stack.push_multiple({30,40,50});
+  BOOST_CHECK_EQUAL(stack.peek(), 50);
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.peek(), 40);
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.peek(), 30);
+  BOOST_CHECK_THROW(stack.pop(), std::out_of_range());
+}
+
+BOOST_AUTO_TEST_CASE(mystack_clear)
+{
+  MyStack<int> stack;
+  stack.push_multiple({30,40,50});
   BOOST_CHECK_EQUAL(stack.size(), 3UL);
- 
-  BOOST_CHECK_EQUAL(
+  stack.clear();
+  BOOST_CHECK_EQUAL(stack.size(), 0UL);
+  stack.clear();
+  BOOST_CHECK_EQUAL(stack.size(), 0UL);
+}
+
+BOOST_AUTO_TEST_CASE(mystack_size)
+{
+  MyStack<int> stack;
+  stack.push(10);
+  BOOST_CHECK_EQUAL(stack.size(), 1UL);
+  stack.push(20);
+  BOOST_CHECK_EQUAL(stack.size(), 2UL);
+  stack.pop();
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.size(), 0UL);
+
+}
+
+BOOST_AUTO_TEST_CASE(mystack_is_empty)
+{
+  MyStack<int> stack;
+  BOOST_CHECK_EQUAL(stack.is_empty(), true);
+  stack.push(10);
+  BOOST_CHECK_EQUAL(stack.is_empty(), false);
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.is_empty(), true);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
